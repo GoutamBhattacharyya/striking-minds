3e03469de0d882fdf3af83c81bbeaec8b4a03d2c
goutam bhattacharyya <goutamb@coachlist.com>
2018-01-14 21:39:54 +0530

    working with browserify

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var reload = browserSync.reload;
 var autoprefixer = require('gulp-autoprefixer');
 var clean = require('gulp-clean');
 var concat = require('gulp-concat');
+var browserify = require('gulp-browserify');
 
 var SOURCEPATHS = {
     sassSource : 'src/scss/*.scss',
@@ -39,6 +40,7 @@ gulp.task('copy',['clean-html'],function(){
 gulp.task('scripts',['clean-scripts'],function(){
     gulp.src(SOURCEPATHS.jsSource)
         .pipe(concat('main.js'))
+        .pipe(browserify())
         .pipe(gulp.dest(APPPATH.js));
 });
 gulp.task('serve', ['sass'], function(){
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,7 @@
-$(alert);
+global.jQuery =  require('jquery');
+bootstrap = require('bootstrap');
+mustache = require('mustache');
+
+jQuery(document).ready(function(){
+    alert("hw");
+});
